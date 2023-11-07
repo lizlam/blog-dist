80f46d93571e37f6f0b546540ba3dd7dad77558b
grepliz <grepliz@pop-os.localdomain>
2023-11-06 22:03:31 -0800

    Add J

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-31d8a482b3802e864833.js"
+    "url": "webpack-runtime-ec63200f7936ac6d5169.js"
   },
   {
     "url": "framework-de9d6b93c2fe39840233.js"
@@ -39,14 +39,14 @@ self.__precacheManifest = [
     "url": "b3a13d7d-9dd20648204066c00742.js"
   },
   {
-    "url": "app-0caa4f0f1fe29587b395.js"
+    "url": "app-2018115bdb4d48aa9853.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ac0c7ba8fb440deab018f1e39e5402d5"
+    "revision": "447c728e398e9ee24accb96548155118"
   },
   {
-    "url": "polyfill-99ac2272fc3492dee395.js"
+    "url": "polyfill-02052622e62bdebc9400.js"
   },
   {
     "url": "manifest.webmanifest",
@@ -160,7 +160,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-0caa4f0f1fe29587b395.js`))) {
+  if (!resources || !(await caches.match(`/app-2018115bdb4d48aa9853.js`))) {
     return await fetch(event.request)
   }
 
